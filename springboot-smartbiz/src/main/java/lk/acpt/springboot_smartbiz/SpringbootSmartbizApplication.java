a1954630d1444db0f7a376688f347261fc6670a6
Hiruni Prarthana <137154585+Hiruni29145@users.noreply.github.com>
2025-08-18 17:48:06 +0530

    Initial Spring Boot project setup

---
+++ springboot-smartbiz/src/main/java/lk/acpt/springboot_smartbiz/SpringbootSmartbizApplication.java
@@ -0,0 +1,13 @@
+package lk.acpt.springboot_smartbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSmartbizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSmartbizApplication.class, args);
+	}
+
+}
---
+++ springboot-smartbiz/src/test/java/lk/acpt/springboot_smartbiz/SpringbootSmartbizApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.acpt.springboot_smartbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSmartbizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
